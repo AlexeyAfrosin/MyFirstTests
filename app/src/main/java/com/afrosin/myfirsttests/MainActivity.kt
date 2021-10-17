737683a02841e049c0b44f922911944023056912
Alexey <a.afrosin@tarifer.ru>
2021-10-17 14:29:00 +0400

    подготовка программы для первого задания

--- app/src/main/java/com/afrosin/myfirsttests/MainActivity.kt
+++ app/src/main/java/com/afrosin/myfirsttests/MainActivity.kt
@@ -2,10 +2,36 @@ package com.afrosin.myfirsttests
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.afrosin.myfirsttests.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val vb: ActivityMainBinding by viewBinding()
+    private val emailValidator = EmailValidator()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        with(vb) {
+            emailInput.addTextChangedListener(emailValidator)
+
+            saveButton.setOnClickListener {
+                if (emailValidator.isValid) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        getString(R.string.valid_email),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    val errorEmail = getString(R.string.invalid_email)
+                    emailInput.error = errorEmail
+                    Toast.makeText(this@MainActivity, errorEmail, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
+
 }
